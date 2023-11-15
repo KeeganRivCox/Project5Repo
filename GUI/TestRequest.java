69c9ab8376504b1f2ea83fd62e29c921fd821755
KeeganRivCox <143858390+KeeganRivCox@users.noreply.github.com>
2023-11-15 17:18:11 -0500

    Initial commit

---
+++ GUI/TestRequest.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class TestRequest {
+
+    public static void main(String[] args) throws IOException {
+
+        try (Socket socket = new Socket("server", 9087)) {
+
+
+
+        }
+
+
+    }
+
+
+}
